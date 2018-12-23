9a098d3857745e622bc574c4786001ccf7afb100
yoosojeong <sksdbthwjd@naver.com>
2018-12-23 13:42:04 +0900

    Updates

---
+++ precache-manifest.fe42bba9822745852c15486f627b43b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b58755edd741b4cd8b47",
+    "url": "/yoosojeong/movie-app/static/js/runtime~main.b58755ed.js"
+  },
+  {
+    "revision": "c64cafc0d373cf0dea02",
+    "url": "/yoosojeong/movie-app/static/js/main.c64cafc0.chunk.js"
+  },
+  {
+    "revision": "49a8c5e4c0e3694e0b30",
+    "url": "/yoosojeong/movie-app/static/js/1.49a8c5e4.chunk.js"
+  },
+  {
+    "revision": "c64cafc0d373cf0dea02",
+    "url": "/yoosojeong/movie-app/static/css/main.9fda11a4.chunk.css"
+  },
+  {
+    "revision": "94272b6c90c7395730ee215f82e471a0",
+    "url": "/yoosojeong/movie-app/index.html"
+  }
+];
